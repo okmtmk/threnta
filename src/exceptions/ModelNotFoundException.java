d43efdf078171e12b19b79cfccae7b34cf717226
okmtmk <c0118060b4+2nd@edu.teu.ac.jp>
2019-12-20 15:43:49 +0900

    [update].gitignore

--- src/exceptions/ModelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class ModelNotFoundException extends Exception {
-    public ModelNotFoundException(String modelName, long id) {
-        super("Model name : " + modelName + ", id : " + id);
-    }
-}
